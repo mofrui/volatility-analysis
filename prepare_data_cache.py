4d4db6d66360594ac37bbad785ad5af3136763b2
SheXinLim <slim0093@uni.sydney.edu.au>
2025-05-19 12:44:45 +1000

    dashbaord streamlit works
    # Conflicts:
    #	dashboard/dashboard.py
    #	dashboard/dashboard_app.py
    #	prepare_data_cache.py

--- prepare_data_cache.py
+++ prepare_data_cache.py
@@ -12,6 +12,7 @@ df_feat = pd.read_parquet("data/order_book_feature.parquet")
 df_tgt = pd.read_parquet("data/order_book_target.parquet")
 
 # Features used for LSTM
+<<<<<<< HEAD
 feature_cols = [
     "wap", "spread_pct", "imbalance", "depth_ratio", "log_return",
     "log_wap_change", "rolling_std_logret", "spread_zscore", "volume_imbalance"
@@ -21,6 +22,17 @@ feature_cols = [
 
 for stock_id in stock_ids:
     pkl_path = f"data/preprocessed_9_{stock_id}.pkl"
+=======
+# feature_cols = [
+#     "wap", "spread_pct", "imbalance", "depth_ratio", "log_return",
+#     "log_wap_change", "rolling_std_logret", "spread_zscore", "volume_imbalance"
+# ]
+feature_cols = ["wap", "log_return"]
+
+
+for stock_id in stock_ids:
+    pkl_path = f"data/preprocessed_{stock_id}.pkl"
+>>>>>>> d1e1c5ba (dashbaord streamlit works)
     if Path(pkl_path).exists():
         print(f"✅ {pkl_path} already exists. Skipping...")
         continue
@@ -36,10 +48,15 @@ for stock_id in stock_ids:
     df = util.add_features(df)
 
     valid_ids = df.groupby("time_id").filter(lambda g: len(g) >= 340)["time_id"].unique()
+<<<<<<< HEAD
     # limited_ids = valid_ids[:100] 
     # df = df[df["time_id"].isin(limited_ids)].copy()
     df = df[df["time_id"].isin(valid_ids)].copy()
 
+=======
+    limited_ids = valid_ids[:100] 
+    df = df[df["time_id"].isin(limited_ids)].copy()
+>>>>>>> d1e1c5ba (dashbaord streamlit works)
 
     lstm_df = util.generate_tick_sequences(df, feature_cols)
     if lstm_df.empty:
